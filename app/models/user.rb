c681ecfaa40e5a0bd346673b2cd3f28973cd9589
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-05 17:49:39 -0500

    add is_student? and is_teacher? methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_one :teacher
   has_one :student
+
+  def is_teacher?
+    teacher ? true : false
+  end
+
+  def is_student?
+    student ? true : false
+  end
 end
