1e8d72886901da3a6019643d75e14c33b17bffcb
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-05 16:40:56 -0500

    add user into seed file; create sessions_controller; can login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def login
+    if request.post?
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+      end
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to sessions_login_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/login'
+  post 'sessions/login'
+  get 'sessions/logout'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
