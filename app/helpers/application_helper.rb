4c953917fe4234d1231a54ccdb36485ca7d5f124
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-06 00:53:21 -0500

    add permission that only let teachers edit assignment descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def teacher?
+    current_user.is_teacher? if current_user
+  end
 end
